6e9b757c5bf64936cb5cbbc2fe339caa6ba8e6e9
xNovo3000 <fontana.ciro.2001@gmail.com>
2024-01-29 22:00:12 +0100

    Tested CryptoManager async case

--- data/crypto/src/androidTest/kotlin/com/github/xnovo3000/gigapass/data/crypto/CryptoManagerTest.kt
+++ data/crypto/src/androidTest/kotlin/com/github/xnovo3000/gigapass/data/crypto/CryptoManagerTest.kt
@@ -6,13 +6,15 @@ import com.google.common.truth.Truth
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.util.concurrent.Executors
 
 @RunWith(AndroidJUnit4ClassRunner::class)
 class CryptoManagerTest {
 
     companion object {
-        const val VALUE = "MyUltraSecretPassword1"
         const val TAG = "CryptoManagerTest"
+        const val TEST_VALUE = "MyUltraSecretPassword"
+        const val TEST_TIMES = 20
     }
 
     private lateinit var cryptoManager: CryptoManager
@@ -23,11 +25,39 @@ class CryptoManagerTest {
     }
 
     @Test
-    fun encryptAndDecryptString() {
-        val encryptedValue = cryptoManager.encrypt(VALUE)
-        Log.d(TAG, "encryptedValue: $encryptedValue")
+    fun encryptAndDecryptStringMultiSync() {
+        // Execute N times
+        for (i in 0 until TEST_TIMES) {
+            // Test
+            encryptAndDecryptString("$TEST_VALUE$i")
+        }
+    }
+
+    @Test
+    fun encryptAndDecryptStringMultiAsync() {
+        // Create code to execute asynchronously
+        val executions = List(TEST_TIMES) {
+            Runnable { encryptAndDecryptString("$TEST_VALUE$it") }
+        }
+        // Create thread pool
+        val executorService = Executors.newFixedThreadPool(TEST_TIMES)
+        // Launch executions
+        executions.forEach { executorService.submit(it) }
+        // Wait for termination
+        executorService.shutdown()
+        while (!executorService.isTerminated) {
+            Thread.sleep(1000)
+        }
+    }
+
+    private fun encryptAndDecryptString(value: String) {
+        // Encrypt
+        val encryptedValue = cryptoManager.encrypt(value)
+        // Decrypt
         val decryptedValue = cryptoManager.decrypt(encryptedValue)
-        Truth.assertThat(decryptedValue).isEqualTo(VALUE)
+        // Check
+        Truth.assertThat(decryptedValue).isEqualTo(value)
+        Log.d(TAG, "Execution with value $value ok")
     }
 
 }
