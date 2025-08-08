d48456d55c31d6088be83cf7884dfa2aca2f6169
xNovo3000 <fontana.ciro.2001@gmail.com>
2025-08-08 15:53:12 +0200

    Added module for keychain page

--- feature/keychain/build.gradle.kts
+++ feature/keychain/build.gradle.kts
@@ -1,29 +1,22 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.kotlin.serialization)
-    alias(libs.plugins.ksp)
-    alias(libs.plugins.hilt)
 }
 
 android {
-    namespace = "com.github.xnovo3000.gigapass"
+    namespace = "com.github.xnovo3000.gigapass.feature.keychain"
     compileSdk = 36
     buildToolsVersion = "36.0.0"
     defaultConfig {
-        applicationId = "com.github.xnovo3000.gigapass"
         minSdk = 26
-        targetSdk = 36
-        versionCode = 1
-        versionName = "0.1.0"
+        consumerProguardFiles("consumer-rules.pro")
     }
     buildTypes {
         release {
-            isMinifyEnabled = true
-            isShrinkResources = true
+            isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
@@ -46,26 +39,19 @@ android {
 
 dependencies {
     // Project
-    api(project(":feature:keychain"))
+    api(project(":core:ui"))
+    api(project(":data:keychain"))
     // AndroidX
     implementation(libs.androidx.core)
-    implementation(libs.androidx.core.splashscreen)
-    implementation(libs.androidx.activity)
     // Jetpack Compose
     implementation(platform(libs.compose.bom))
     implementation(libs.compose.animation)
     implementation(libs.compose.foundation)
     implementation(libs.compose.runtime)
     implementation(libs.compose.ui)
-    // Lifecycle
-    implementation(libs.lifecycle.viewmodel)
-    implementation(libs.lifecycle.viewmodel.navigation3)
-    // Hilt
-    implementation(libs.hilt.android)
-    ksp(libs.hilt.compiler)
-    // KotlinX
-    implementation(libs.kotlinx.serialization.core)
-    // Navigation3
-    implementation(libs.navigation3.runtime)
-    implementation(libs.navigation3.ui)
+    // Jetpack Compose - Preview
+    implementation(libs.compose.ui.preview)
+    debugImplementation(libs.compose.ui.tooling)
+    // Jetpack Compose - Material 3 Expressive
+    implementation(libs.compose.material3)
 }
