f9019699d0973748b72cbf1bef339b061d1b750f
xNovo3000 <fontana.ciro.2001@gmail.com>
2025-08-12 11:36:36 +0200

    Finished viewmodel

--- feature/key/src/main/kotlin/com/github/xnovo3000/gigapass/feature/key/viewmodel/KeyViewModel.kt
+++ feature/key/src/main/kotlin/com/github/xnovo3000/gigapass/feature/key/viewmodel/KeyViewModel.kt
@@ -1,33 +1,23 @@
 package com.github.xnovo3000.gigapass.feature.key.viewmodel
 
-import android.util.Log
 import androidx.compose.foundation.text.input.TextFieldState
 import androidx.compose.foundation.text.input.setTextAndPlaceCursorAtEnd
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.github.xnovo3000.gigapass.core.crypto.CryptoManager
-import com.github.xnovo3000.gigapass.data.keychain.entity.KeyEntity
-import com.github.xnovo3000.gigapass.data.keychain.repository.KeyRepository
+import com.github.xnovo3000.gigapass.feature.key.domain.RetrieveDecryptedKeyUseCase
+import com.github.xnovo3000.gigapass.feature.key.domain.UpdateKeyUseCase
+import com.github.xnovo3000.gigapass.feature.key.dto.DecryptedKeyDto
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import javax.inject.Inject
 
 @HiltViewModel(assistedFactory = KeyViewModelFactory::class)
 class KeyViewModel @AssistedInject constructor(
     @Assisted private val keyId: Long,
-    private val keyRepository: KeyRepository,
-    private val cryptoManager: CryptoManager
+    private val retrieveDecryptedKeyUseCase: RetrieveDecryptedKeyUseCase,
+    private val updateKeyUseCase: UpdateKeyUseCase
 ) : ViewModel() {
-
-    companion object {
-        private const val TAG = "KeyViewModel"
-    }
-
-    var isIdValid: Boolean = false
 
     val websiteApplicationTextFieldState = TextFieldState()
     val usernameTextFieldState = TextFieldState()
@@ -37,34 +27,24 @@ class KeyViewModel @AssistedInject constructor(
 
     fun loadDefaults() {
         viewModelScope.launch {
-            withContext(Dispatchers.Default) {
-                // Get keyEntity
-                val keyEntity = keyRepository.getById(keyId)
-                if (keyEntity == null) {
-                    Log.e(TAG, "Key with ID $keyId does not exists")
-                    return@withContext
-                }
-                // Try to decrypt password
-                val decryptedPasswordResult = cryptoManager.decrypt(keyEntity.encryptedPassword)
-                if (decryptedPasswordResult.isFailure) {
-                    Log.e(TAG, "Failed to decrypt the password. As a safety measure this key is not valid anymore")
-                    return@withContext
-                }
-                // Set as valid entity
-                isIdValid = true
-                // Set default values
-                websiteApplicationTextFieldState.setTextAndPlaceCursorAtEnd(keyEntity.serviceName)
-                usernameTextFieldState.setTextAndPlaceCursorAtEnd(keyEntity.username)
-                passwordTextFieldState.setTextAndPlaceCursorAtEnd(decryptedPasswordResult.getOrThrow())  // Will never throw
+            retrieveDecryptedKeyUseCase(id = keyId)?.let {
+                websiteApplicationTextFieldState.setTextAndPlaceCursorAtEnd(it.serviceName)
+                usernameTextFieldState.setTextAndPlaceCursorAtEnd(it.username)
+                passwordTextFieldState.setTextAndPlaceCursorAtEnd(it.password)
             }
         }
     }
 
     fun apply() {
         viewModelScope.launch {
-            withContext(Dispatchers.Default) {
-
-            }
+            updateKeyUseCase(
+                decryptedKeyDto = DecryptedKeyDto(
+                    id = keyId,
+                    serviceName = websiteApplicationTextFieldState.text.toString(),
+                    username = usernameTextFieldState.text.toString(),
+                    password = passwordTextFieldState.text.toString()
+                )
+            )
         }
     }
 
